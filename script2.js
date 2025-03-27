d7bb467f2db56f8e67f479b27e1d8e9d2b386552
Georg von Gruben <gvongruben@deba.de>
2025-03-27 15:06:10 +0100

    Initial commit

---
+++ script2.js
@@ -0,0 +1,36 @@
+// function multiply(a, b){
+//     return a*b
+// }
+// var erst=prompt("Bitte erste Zahl eingeben");
+// var zweit=prompt("Bitte zweite Zahl eingeben");
+// var total = multiply(erst, zweit);
+// console.log(multiply(erst,zweit));
+// console.log(total);
+var animals = [["Tiger", "cat", "bear", "Bird"]];
+// console.log(animals[0][2])
+// var animals = ["Tiger", "cat", "bear", "Bird"];
+// console.log(animals[0])
+// animals.shift();
+// animals.pop();
+// var animals_big = animals.concat(["bee","bear"])
+// console.log(animals);
+// console.log(animals_big);
+// console.log(animals_big[0])
+// console.log(animals_big[1])
+// console.log(animals_big[2])
+
+var user= {
+    username:"Georg",
+    age:36,
+    isMarried:false,
+};
+console.log(user.age)
+
+console.log(user)
+user.facouriteFood="Apples";
+console.log(user.facouriteFood)
+
+
+
+
+document
